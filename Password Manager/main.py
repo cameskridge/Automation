16bdba238edde86e61d19294e3e1eb18dd08afcc
cameskridge <47401339+cameskridge@users.noreply.github.com>
2025-01-31 17:08:50 -0500

    Add files via upload

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,24 +1,50 @@
 from tkinter import *
+from tkinter import messagebox
 import string
 import random
 
+CAPS = string.ascii_uppercase
+NUMBERS = '1234567890'
+SPECIAL_CHARS = '!@#$%^&*-_=+.'
+
 #PASSWORD GENERATOR
 def generate_password():
-    characters = string.ascii_uppercase+string.ascii_lowercase+'!@#$%^&*-_=+.'
-    password = random.choice(string.ascii_uppercase)+random.choice('1234567890')
+    characters = string.ascii_uppercase+string.ascii_lowercase+SPECIAL_CHARS
+    password = random.choice(string.ascii_uppercase)+random.choice(NUMBERS)
     for i in range(11):
         password+=random.choice(characters)
     password_form.delete(0, END)
     password_form.insert(0, password)    
+
+#VALIDATE PASSWORD
+def is_password_valid():
+    if not any(char in SPECIAL_CHARS for char in password_form.get()):
+        return False
+    elif not any(i in CAPS for i in password_form.get()):
+        return False
+    elif not any(i in NUMBERS for i in password_form.get()):
+        return False
+    else: 
+        return True 
     
 #SAVE PASSWORD
 def export_login():
-    login = f'{website_form.get()} | {email_form.get()} | {password_form.get()}'
-    file_name = "passwords.txt"
-    with open(file_name, "a") as file:
-        file.write(login + "\n")
-    website_form.delete(0, END)
-    password_form.delete(0, END)
+    if not is_password_valid():
+        messagebox.showinfo("Error", "Password should include a capital letter, number, and special character.")
+    elif len(website_form.get()) < 1:
+        messagebox.showinfo("Error", "Please enter a website name.")
+    elif len(password_form.get()) < 6:
+        messagebox.showinfo("Error", "Password should be at least six digits.")
+    elif len(email_form.get()) < 1:
+        messagebox.showinfo("Error", "Please enter an email address.")
+    else:
+        login = f'{website_form.get()} | {email_form.get()} | {password_form.get()}'
+        file_name = "passwords.txt"
+        with open(file_name, "a") as file:
+            file.write(login + "\n")
+        website_form.delete(0, END)
+        password_form.delete(0, END)
+        messagebox.showinfo("Success", "Your login has been saved!")
 
 #USER INTERFACE
 window = Tk()
